ada231899d005939b3361d2e56c41b2902a412f9
Ryszard Szewczyk <ryszardszewczyk2204@gmail.com>
2023-05-10 23:10:12 +0200

    Hotfix - Dodanie komentarza nad enumem dla SingularField, gdyż to jest false-positive

--- service/shop-domain-common/src/main/java/pl/com/web/shop/domain/api/ErrorCodes.java
+++ service/shop-domain-common/src/main/java/pl/com/web/shop/domain/api/ErrorCodes.java
@@ -1,11 +1,16 @@
 package pl.com.web.shop.domain.api;
 
+import javax.validation.constraints.NotBlank;
+
+@SuppressWarnings("PMD.SingularField")
 public enum ErrorCodes {
     OBJECT_NOT_FOUND("OBJECT_NOT_FOUND"),
     VERSION_MISMATCH("VERSION_MISMATCH");
+
+    @NotBlank
     private String value;
 
-    ErrorCodes(String value) {
+    ErrorCodes(@NotBlank String value) {
         this.value = value;
     }
 }
